592c1158e74f865f1182785e7fa8e216579354aa
Kaveesha <kaveeshewaduge111@gmail.com>
2024-12-19 12:07:01 +0530

    sanity setup

--- startup-next/app/(root)/page.tsx
+++ startup-next/app/(root)/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams, }: {
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const posts = [{
+    _createdAt: new Date(),
+  }];
 
   return (
     <>
@@ -15,6 +19,22 @@ export default async function Home({ searchParams, }: {
          </p>
          
          <SearchForm query={query} />
+       </section>
+
+       <section className="section_container">
+         <p className="text-30-semibold">
+            {query? `Search results for "${query}"` : 'All Startups'}
+         </p>
+
+         <ul className="mt-7 card_grid">
+            {posts?.length > 0 ? (
+              posts.map((post: StartupTypeCard) => (
+                <StartupCard key={post?._id} post={post} />
+              ))
+            ): (
+              <p className="no-results">No startups found</p>
+            )}
+         </ul>
        </section>
     </>
   );
